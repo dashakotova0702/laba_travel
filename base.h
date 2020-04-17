41eed44d4f871fb83c2c57d23ebce3ddd78fddaf
dashakotova0702 <dashakotova0702@gmail.com>
2020-04-17 23:54:09 +0300

    Попытка сдать лабу: 1

--- base.h
+++ base.h
@@ -1,14 +1,34 @@
 #include <string>
+#include <iostream>
 using namespace std;
 
 class tours {
 public:
-  void setTours();
-  void read();
-  void reserv();
-  void del();
-  void search();
+  void setTours(fstream& namefile);
+  void read(fstream& namefile);
+  void reserv(fstream& namefile);
+  void del(fstream& namefile);
+  void search(fstream& namefile);
   int menu();
+  friend ostream& operator<< (ostream &out, const tours &t) {
+    out << t.country << ' ' << t.town << ' ' << t.hotel << " Звезд: " << t.stars << " Мест в отеле: " << t.places << " Свободных мест: " << t.freeplaces << endl;
+    return out;
+  }
+  friend istream& operator>> (istream &in, tours &t) {
+    cout << "Введите страну: " << endl;
+    in >> t.country;
+    cout << "Введите город: " << endl;
+    in >> t.town;
+    cout << "Введите отель: " << endl;
+    in >> t.hotel;
+    cout << "Сколько звезд у отеля: " << endl;
+    in >> t.stars;
+    cout << "Сколько мест в отеле: " << endl;
+    in >> t.places;
+    cout << "Сколько свободных мест в отеле: " << endl;
+    in >> t.freeplaces;
+    return in;
+  }
 private:
   string country;
   string town;
--- main.cpp
+++ main.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
+#include <fstream>
 #include "base.h"
 using namespace std;
 
 int main(int argc, char const *argv[]) {
-  if (argc != 1) {
+  if (argc != 2) {
     cout << "Неверное количество параметров" << endl;
+    return 1;
+  }
+  fstream file;
+  file.open(argv[1], ios_base::binary | ios_base::app | ios_base::in);
+  if (!(file.is_open())){
+    cout << "Ошибка открытия файла" << endl;
     return 1;
   }
   int choice;
@@ -13,21 +20,24 @@ int main(int argc, char const *argv[]) {
     choice = t->menu();
     switch(choice) {
       case 1:
-        t->setTours();
+        t->setTours(file);
         break;
       case 2:
-        t->read();
+        file.seekg(0);
+        t->read(file);
         break;
 /*      case 3:
-        t->search();
+        t->search(file);
 			  break;
 			case 4:
-        t->del();
+        t->del(file);
 				break;
 			case 5:
-        t->reserv();
+        t->reserv(file);
 				break;*/
 			case 6:
+        file.close();
+        delete t;
         exit(0);
 			default:
         cout << "Неправильно введено значение" << endl;
